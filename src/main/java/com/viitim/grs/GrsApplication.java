7e08a3ccdd52e75fe4d419fdd252c699af88a418
T. Jääskeläinen <timo.a.jaaskelainen@helsinki.fi>
2017-08-30 18:14:35 +0300

    o

--- src/main/java/com/viitim/grs/GrsApplication.java
+++ src/main/java/com/viitim/grs/GrsApplication.java
@@ -1,12 +1,44 @@
 package com.viitim.grs;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@EnableAutoConfiguration
+@ComponentScan
 @SpringBootApplication
-public class GrsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GrsApplication.class, args);
-	}
+public class GrsApplication extends SpringBootServletInitializer {
+ 
+	@Autowired
+	Example example;
+	
+    public static void main(String[] args) {
+        SpringApplication.run(applicationClass, args);
+    }
+ 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+ 
+    private static Class<GrsApplication> applicationClass = GrsApplication.class;
 }
+@RestController
+@EnableAutoConfiguration
+ class Example {
+
+	   @RequestMapping("/hello")
+	    String hello() {
+	 
+	        return "Hi !";
+	 
+	    } 
+    }
