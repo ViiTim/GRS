80ccc1c2a2f0e75e37b711ffb806aff5d924fa36
T. Jääskeläinen <timo.a.jaaskelainen@helsinki.fi>
2017-08-30 18:45:38 +0300

    server

--- src/main/java/com/viitim/grs/GrsApplication.java
+++ src/main/java/com/viitim/grs/GrsApplication.java
@@ -1,25 +1,21 @@
 package com.viitim.grs;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @EnableAutoConfiguration
 @ComponentScan
 @SpringBootApplication
 public class GrsApplication extends SpringBootServletInitializer {
  
-	@Autowired
-	Example example;
-	
     public static void main(String[] args) {
         SpringApplication.run(applicationClass, args);
     }
@@ -31,14 +27,14 @@ public class GrsApplication extends SpringBootServletInitializer {
  
     private static Class<GrsApplication> applicationClass = GrsApplication.class;
 }
-@RestController
-@EnableAutoConfiguration
- class Example {
 
-	   @RequestMapping("/hello")
-	    String hello() {
-	 
-	        return "Hi !";
-	 
-	    } 
+@RestController
+class HelloController {
+ 
+    @RequestMapping("/hello/{name}")
+    String hello(@PathVariable String name) {
+ 
+        return "Hi " + name + " !";
+ 
     }
+}
